04bf8b0f175aab516eb9a1edb0241272c7df35c7
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2025-05-25 00:15:11 -0400

    Add project files.

---
+++ Quick_Sort.py
@@ -0,0 +1,13 @@
+﻿def quick_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    pivot = lst[len(lst) // 2]
+    left = [x for x in lst if x < pivot]
+    middle = [x for x in lst if x == pivot]
+    right = [x for x in lst if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+# تجربة الفرز
+numbers = [33, 10, 68, 19, 27, 5]
+sorted_numbers = quick_sort(numbers)
+print("القائمة بعد الفرز:", sorted_numbers)
